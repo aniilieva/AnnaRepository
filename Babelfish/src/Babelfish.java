127443138ceb64b4ea48bc114b03072167151285
aniilieva <ani.ilieva356@gmail.com>
2019-10-09 15:27:05 +0300

    First Commit for BabelFish by Anna Ilieva

---
+++ Babelfish/src/Babelfish.java
@@ -0,0 +1,99 @@
+
+public class Babelfish {
+
+	String text = "";
+
+	static String PLACEHOLDER = "#";
+
+	public Babelfish(String text) {
+		super();
+		this.text = text;
+	}
+
+
+	public void print(String newLine) {          // Print Method
+
+		System.out.println(newLine);
+
+	}
+
+	public String doMethod() {                               // Logic method 
+
+		if (text.length() >= 3) {
+			
+			
+			text = text.substring(0, 3);
+			
+		} else if (text.length() < 3) {
+			
+
+			
+			text = text.substring(0, text.length());
+
+			if (text.length() == 1) {
+				text = text + PLACEHOLDER + PLACEHOLDER;
+			}
+
+			else if (text.length() == 2) {
+				text = text + PLACEHOLDER;
+			}
+			
+			else if (text.length()==0) {
+				text = PLACEHOLDER + PLACEHOLDER + PLACEHOLDER;
+			}
+		} 
+
+		return text;
+	}
+
+	// ---------------------> 
+	
+	
+	public String doMethod(int numberOfSymbol, char placeholder) {
+
+		char[] text2 = text.toCharArray();
+
+		String newLine = "";
+
+		for (int i = 0; i < text.length(); i++) { // foor loop always will be executed to text,lenght
+
+//			
+
+			if (i < numberOfSymbol) {
+
+				newLine = newLine + text2[i]; // from 1 do text.lenght
+
+			}
+
+			else {
+
+				newLine = newLine + placeholder;
+			}
+
+		}
+
+		if (numberOfSymbol > text.length()) {
+
+			for (int i = 0; i < (numberOfSymbol) - (text.length()); i++) {
+
+				newLine = newLine + placeholder;
+			}
+
+		}
+
+		return newLine;
+
+	}
+
+	public static void main(String[] args) { // Main method
+
+		Babelfish p1 = new Babelfish("tenis");  //5 broq 
+
+	String newLine = p1.doMethod(3,'%');
+	p1.print(newLine);
+		 newLine = p1.doMethod(5, '#');
+		p1.print(newLine);
+
+	}
+
+}
